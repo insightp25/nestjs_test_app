d641f39c82e74c7504d93fc84db9ff60a6b313f8
JO <71416000+insightp25@users.noreply.github.com>
2025-11-15 03:12:22 +0900

    added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
+export const metadata: Metadata = {
+  title: "next.js test app build",
+  description: "next.js 튜토리얼 테스트앱 개발하기"
+};
+
 export default function Home() {
-  return <main>Home
+
+  return 
+  <main>
+    Home
     <Link href="/dashboard">go to dashboard</Link>
   </main>;
 }
