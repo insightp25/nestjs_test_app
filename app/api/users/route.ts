53a46eda31cdb158896ebe3015e0951e2f0715ed
JO <71416000+insightp25@users.noreply.github.com>
2025-11-15 01:51:19 +0900

    fetch 활용 api 구현

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,11 +1,16 @@
 import { NextResponse } from "next/server";
 
+const DB = [
+    {id: 1, name: "daisy"},
+    {id: 2, name: "tom"},
+    {id: 3, name: "michael"},
+]
+
 export async function GET(request: Request) {
+    const searchParams = new URL(request.url).searchParams;
+    const name = searchParams.get("name") as string;
+
     return NextResponse.json({
-        users: [
-            {id: 1, name: "daisy"},
-            {id: 2, name: "tom"},
-            {id: 3, name: "michael"},
-        ]
-    })
+        users: DB.filter((user) => user.name.includes(name))
+    });
 }
